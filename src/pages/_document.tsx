33bb21c14835106151f99896f0a1ce2506f03dae
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2025-03-09 22:13:20 +0800

    vercel-analytics-updated

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,12 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
+import { Analytics } from '@vercel/analytics/next';
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       <body className="antialiased overflow-x-hidden">
+        <Analytics />
         <Main />
         <NextScript />
       </body>
